dfaffda6d44836232de1b5bf4412048370ab64b2
cappydev <kevin.lanel@wanadoo.fr>
2019-02-05 16:02:54 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-
-$(document).ready(function(){
-    $( "#clickMe" ).click(function() {
-        alert( 'ça fonctionne' );
-    });
+$(function(){ // you can wrap it here with in document ready block
+   var count = 0;
+   $('#button').click(function(){
+    count++; //ajoute 1 au compteur
+      $('#text').val(count);//fonction val permet de définir la valeur d'un input (pas forcément que des nombres)
+   });
 });
